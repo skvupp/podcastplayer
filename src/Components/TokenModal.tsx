17e2fa4a05de8cde6f2ae2726921bda615a99780
yngve <yngve@skvupp.com>
2022-06-14 22:49:31 +0200

    Modal descriptions

--- src/Components/TokenModal.tsx
+++ src/Components/TokenModal.tsx
@@ -22,11 +22,16 @@ export function TokenModal(props: Props): JSX.Element {
     };
 
     return <Modal visible={visible} footer={false} closeIcon={false} closable={false} width={700}>
+        <h2>This is a protected episode</h2>
+        <p>A protected episode needs an authorization token to be played.
+        </p>
         <Form name="nest-messages" onFinish={onFinish} >
-            <Form.Item required label="Lim inn kode" name="token">
+            <Form.Item required label="Paste authorization code here" name="token">
                 <Input.TextArea />
             </Form.Item>
-            <p><a href={link} target="_blank" rel="noreferrer">Get code to open episode</a></p>
+            <p>
+                <a href={link} target="_blank" rel="noreferrer">Get token to open episode</a><br/>
+            </p>
             <Form.Item>
                 <Button style={{marginRight: '20px'}} onClick={onClose}>Close</Button>
                 <Button type="primary" htmlType="submit">
